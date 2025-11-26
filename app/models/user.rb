480a2ac254241991c2882834ceca74878e27e246
glicina-24 <glicina0714a@gmail.com>
2025-11-26 14:26:57 +0900

    Userモデル作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,  presence: true
+  validates :email, presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            length: { maximum: 255 }
+end
