e12bf841909ab8b9f3de2ee244ed2e2a55e5893a
glicina-24 <glicina0714a@gmail.com>
2025-12-01 14:57:43 +0900

    shopsコントローラー作成

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,43 @@
+class ShopsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    # ひとまず全部表示。あとで「公開フラグ」とか付けてもOK
+    @shops = Shop.includes(:user).order(created_at: :desc)
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
+  end
+
+  def new
+    @shop = current_user.shops.build
+  end
+
+  def create
+    @shop = current_user.shops.build(shop_params)
+
+    if @shop.save
+      redirect_to @shop, notice: "店舗を登録しました。"
+    else
+      flash.now[:alert] = "店舗登録に失敗しました。入力内容を確認してください。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(
+      :name,
+      :description,
+      :address,
+      :prefecture,
+      :city,
+      :postal_code,
+      :phone_number,
+      :latitude,
+      :longitude
+    )
+  end
+end
