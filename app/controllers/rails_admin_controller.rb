5d2679680afd966747d8041e741adb389fc879fe
glicina-24 <glicina0714a@gmail.com>
2026-01-05 16:58:41 +0900

    一般ユーザーが管理画面にアクセスした場合のエラー修正

--- app/controllers/rails_admin_controller.rb
+++ app/controllers/rails_admin_controller.rb
@@ -3,6 +3,14 @@ class RailsAdminController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  # Devise
   before_action :authenticate_user!
+  before_action :require_admin!
+
+  private
+
+  def require_admin!
+    return if current_user&.admin?
+
+    redirect_to main_app.root_path, alert: "権限がありません"
+  end
 end
