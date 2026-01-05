86e0d29462d5ed0195459546fc9f556a45057b28
glicina-24 <glicina0714a@gmail.com>
2026-01-05 10:44:58 +0900

    レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   root "homes#index"
   devise_for :users
   resource :mypage, only: %i[show], controller: "mypage"
