96392eef118fecb7512996c92795a9787ef90172
glicina-24 <glicina0714a@gmail.com>
2026-01-13 07:34:20 +0900

    独自ドメイン取得、設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   root "homes#index"
   devise_for :users
 
