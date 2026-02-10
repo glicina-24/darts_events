6f6c012cafc556543ddbe69ddd0069b6e75714af
glicina-24 <glicina0714a@gmail.com>
2026-02-10 23:45:13 +0900

    Sentry送信をproductionのみに制限

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,5 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.environment = ENV.fetch("SENTRY_ENVIRONMENT", Rails.env)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.enabled_environments = %w[production]
 end
