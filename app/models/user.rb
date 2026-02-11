b28edc9188197a94f5063f40f5871ebaebb06aca
glicina-24 <glicina0714a@gmail.com>
2026-02-11 17:14:16 +0900

    devise confirmableを導入してメール認証を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   validates :name, presence: true
   has_many :shops, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_22_231749) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_11_075258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -152,6 +152,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_22_231749) do
     t.string "pro_sns_url"
     t.datetime "pro_applied_at"
     t.integer "role", default: 0, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
